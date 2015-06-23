ccaa5fd0ddf6ad9e59453191dab64593908515c4
TheSystem-69 <ethancmk@hotmail.fr>
2015-06-23 11:12:35 +0200

    poo

--- js/jeu.js
+++ js/jeu.js
@@ -3,17 +3,15 @@ var CODE_TOUCHE_DROITE = 39;
 var ALLER_GAUCHE = false;
 var ALLER_DROITE = false;
 var joueur;
-var numero;
+var code;
 var requestAnimId;
 var window;
 var document;
 
 var fond_score = new Image();
 fond_score.src = 'img/fondgame.svg';
-
 var fond_jeu = new Image();
 fond_jeu.src = 'img/ecranbureau.svg';
-
 var fond_ecran = new Image();
 fond_ecran.src = 'img/fondecran.svg';
 
@@ -24,9 +22,21 @@ var Fondlargeur = 1024;
 var Fondhauteur = 768;
 var couleurFond = "#000000";
 
+var score = 0;
+var affichage_score = 'Score : '+score.toString();
+var posXScore = 440;
+var posYScore = 55;
+var posXScore_final = 340;
+var posYScore_final = 350;
+var posXTour = 385;
+var posYTour = 100;
+var tour = 3;
+var compteur_tour = 'Tour restant : '+tour.toString();
+var couleurScore = '#FFFFFF';
+var affichage_score_final;
+
 var perso = function (Fondlargeur) {
     "use strict";
-    var that = this;
     this.hauteur = 400;
     this.largeur = 400;
     this.positionX = Fondlargeur/2 - this.hauteur/2;
@@ -52,19 +62,55 @@ var perso = function (Fondlargeur) {
     };
 };
 
+var objetDescend = function () {
+    this.posXDroite = 538;
+    this.posYDroite = 0;
+    this.largeurObjet = 330;
+    this.hauteurObjet = 50;
+    this.posXGauche = 158;
+    this.posYGauche = 0;
+    this.img_code_D = new Image();
+    this.img_code_G = new Image();
+    this.ObjetD;
+    this.ObjetG;
+    this.numero;
 
-var score = 0;
-var affichage_score = 'Score : '+score.toString();
-var posXScore = 440;
-var posYScore = 55;
-var posXScore_final = 340;
-var posYScore_final = 350;
-var posXTour = 385;
-var posYTour = 100;
-var tour = 3;
-var compteur_tour = 'Tour restant : '+tour.toString();
-var couleurScore = '#FFFFFF';
-var affichage_score_final;
+    this.getRandomInt = function (min, max) {
+            "use strict";
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    this.random = function () {
+            "use strict";
+            if (Math.random() < 0.5) {
+                this.numero = this.getRandomInt(1, 10);
+                this.ObjetD = true;
+                this.img_code_D.src = 'img/bon_code/b' + this.numero + '.svg';
+                this.img_code_G.src = 'img/mauvais_code/m' + this.numero + '.svg';
+            }
+            else {
+                this.numero = this.getRandomInt(1, 10);
+                this.ObjetG = true;
+                this.img_code_D.src = 'img/mauvais_code/m' + this.numero + '.svg';
+                this.img_code_G.src = 'img/bon_code/b' + this.numero + '.svg';
+            }
+    };
+
+    this.dessiner = function () {
+            "use strict";
+            canvasCodeContext.fillRect (this.posXDroite, this.posYDroite, this.largeurObjet, this.hauteurObjet);
+            canvasCodeContext.drawImage(this.img_code_D, this.posXDroite, this.posYDroite, this.largeurObjet, this.hauteurObjet);
+
+            canvasCodeContext.fillRect (this.posXGauche, this.posYGauche, this.largeurObjet, this.hauteurObjet);
+            canvasCodeContext.drawImage(this.img_code_G, this.posXGauche, this.posYGauche, this.largeurObjet, this.hauteurObjet);
+    };
+
+    this.animer = function () {
+            "use strict";
+            this.posYDroite+=1;
+            this.posYGauche+=1;
+    };
+};
 
 var dessinerScore = function () {
         "use strict";
@@ -85,53 +131,6 @@ var afficher_page_scoreFinal = function (affichage_score_final) {
     canvasPersosContext.font = '25pt serif';
     canvasPersosContext.fillStyle = '#FFFFFF';
     canvasPersosContext.fillText ('Rejouer ?', 445, 550);
-};
-
-var posXObjetD = 540;
-var posYObjetD = 0;
-var largeurObjet = 330;
-var hauteurObjet = 50;
-var posXObjetG = 158;
-var posYObjetG = 0;
-var ObjetD;
-var ObjetG;
-var img_code_D = new Image();
-var img_code_G = new Image();
-
-var getRandomInt = function (min, max) {
-        "use strict";
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-var randomObjet = function () {
-        "use strict";
-        if (Math.random() < 0.5) {
-        numero = getRandomInt(1, 10);
-        ObjetD = true;
-        img_code_D.src = 'img/bon_code/b' + numero + '.svg';
-        img_code_G.src = 'img/mauvais_code/m' + numero + '.svg';
-        }
-        else {
-        numero = getRandomInt(1, 10);
-        ObjetG = true;
-        img_code_D.src = 'img/mauvais_code/m' + numero + '.svg';
-        img_code_G.src = 'img/bon_code/b' + numero + '.svg';
-        }
-};
-
-var dessinerObjet = function () {
-        "use strict";
-        canvasCodeContext.fillRect (posXObjetD, posYObjetD, largeurObjet, hauteurObjet);
-        canvasCodeContext.drawImage(img_code_D, posXObjetD, posYObjetD, largeurObjet, hauteurObjet);
-
-        canvasCodeContext.fillRect (posXObjetG, posYObjetG, largeurObjet, hauteurObjet);
-        canvasCodeContext.drawImage(img_code_G, posXObjetG, posYObjetG, largeurObjet, hauteurObjet);
-};
-
-var animerObjet = function () {
-        "use strict";
-        posYObjetD+=1;
-        posYObjetG+=1;
 };
 
 var onClick = function () {
@@ -164,11 +163,11 @@ var onClick = function () {
 
 var reinitialisation = function () {
         "use strict";
-        ObjetD = false;
-        ObjetG = false;
-        posYObjetD = 0;
-        posYObjetG = 0;
-        randomObjet();
+        code.ObjetD = false;
+        code.ObjetG = false;
+        code.posYDroite = 0;
+        code.posYGauche = 0;
+        code.random();
         joueur.positionFinal = '';
         joueur.positionX = Fondlargeur/2 - joueur.hauteur/2;
         affichage_score = 'Score : '+score.toString();
@@ -200,7 +199,8 @@ var creerCanvasContext = function (name, width, height, zindex, color) {
 var initialisation = function () {
         "use strict";
         joueur = new perso(Fondlargeur);
-        randomObjet();
+        code = new objetDescend();
+        code.random();
         canvasCodeContext = creerCanvasContext("canvasCode", Fondlargeur, Fondhauteur, 1);
         canvasCodeContext.drawImage(fond_ecran, 110, 100);
         canvasFondContext = creerCanvasContext("canvasFond", Fondlargeur, Fondhauteur, 2);
@@ -220,20 +220,20 @@ var principale = function () {
     dessinerScore();
     }
     if (tour <= 0){
-    affichage_score_final = 'Votre score est : '+score.toString();
-    afficher_page_scoreFinal(affichage_score_final);
+        affichage_score_final = 'Votre score est : '+score.toString();
+        afficher_page_scoreFinal(affichage_score_final);
     }
     else {
-    if (posYObjetD < 400) {
-        dessinerObjet();
-        animerObjet();
+    if (code.posYDroite < 400) {
+        code.dessiner();
+        code.animer();
     }
-    else if (ObjetD === true && joueur.positionFinal == 'droite'){
+    else if (code.ObjetD === true && joueur.positionFinal == 'droite'){
         score += 1;
         tour-=1;
         reinitialisation();
     }
-    else if (ObjetG === true && joueur.positionFinal == 'gauche'){
+    else if (code.ObjetG === true && joueur.positionFinal == 'gauche'){
         score += 1;
         tour-=1;
         reinitialisation();
